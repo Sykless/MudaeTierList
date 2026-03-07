d295868d3e6d8822666ae3bcc33015ce38345bf7
Fra <rakouett-du-56@hotmail.fr>
2026-03-07 12:08:21 +0100

    Fix SVG error

--- src/svg/CopyIcon.tsx
+++ src/svg/CopyIcon.tsx
@@ -1,9 +1,7 @@
 export function CopyIcon() {
     return (
-        <svg viewBox = "0 0 24 24" width = "20" height = "20" fill = "currentColor">
-            <path d = {`M16 1H6a2 2 0 0 0-2 2v12h2V3h10V1zm3 4H10a2 2 0 0 0-2
-                2v14a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2zm0 16H10V7h9v14z
-                2 2h12a2 2 0 0 0 2-2V8zm0 2.5L18.5 9H14z`}/>
+        <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
+            <path d="M16 1H6a2 2 0 0 0-2 2v12h2V3h10V1zm3 4H10a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2zm0 16H10V7h9v14z"/>
         </svg>
     )
 }
